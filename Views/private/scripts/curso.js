04a345e9a12327ded9a8f2a900610fee9e9d09f8
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-03-29 02:08:58 -0300

    Criando funcionalidades aps botões

--- Views/private/scripts/curso.js
+++ Views/private/scripts/curso.js
@@ -1,7 +1,7 @@
 
 const urlBackend = "http://localhost:5000";
 
-function consultarCurso(){
+function obterProfessores(){
 
     const elementoSelectProfessor = document.getElementById("professor");
 
@@ -73,7 +73,11 @@ function buscarCursos(){
                             <td>${curso.descricao}</td>
                             <td>${curso.cargaHoraria}</td>
                             <td>${curso.professor.nome}</td>
-                            <td><button class="btn btn-warning" onclick="selecionarCurso">Selecionar</button></td>
+                            <td><button class="btn btn-warning" onclick="selecionarCurso(${curso.id}, 
+                                                                                        '${curso.nome}', 
+                                                                                        '${curso.descricao}', 
+                                                                                        ${curso.cargaHoraria}, 
+                                                                                        ${curso.professor.id})">Selecionar</button></td>
 
                         `;
                         
@@ -92,9 +96,103 @@ function buscarCursos(){
         mostrarMensagem("danger", "Erro ao obter lista de cidade!" + erro);
     });
         
-}         
+} 
 
-function mostrarMensagem(tipo ="sucess", mensagem = "Mensagem Padrão") {
+function cadastrarCurso(){
+    const formCurso = document.getElementById("formCurso");
+    if(formCurso.checkValidity()){
+            const curso = {
+            nome: document.getElementById("nomeCurso").value,
+            descricao: document.getElementById("descricao").value,
+            cargaHoraria: Number(document.getElementById("cargaHoraria").value),
+            professor:{
+                id: document.getElementById("professor").value
+            }
+        }
+        fetch(urlBackend + "/cursos", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(curso)
+        })
+        .then((resposta) => {
+            if(resposta.ok){
+                return resposta.json();
+            }
+        })
+        .then((conteudoJSON) => {
+            if(conteudoJSON.status){
+                mostrarMensagem("success", conteudoJSON.mensagem);
+                limparFormulario();
+                buscarCursos();
+            }
+            else{
+                mostrarMensagem("danger", conteudoJSON.mensagem)
+            }
+        })        
+        .catch((erro) => {
+            mostrarMensagem("danger", "Erro ao obter lista de cidade!" + erro);
+        });
+    }
+    else{
+        formCurso.classList.add("was-validated");
+    }
+}
+    
+function excluirCurso(id){
+    fetch(urlBackend + "/cursos/" + id, {
+        method: "DELETE"
+    })
+     .then((resposta) => {
+        if(resposta.ok){
+            return resposta.json();
+        }
+    })
+    .then((conteudoJSON) => {
+        if(conteudoJSON.status){
+            mostrarMensagem("success", conteudoJSON.mensagem);
+            limparFormulario();
+            resetarBotoes();
+
+            buscarCursos();
+        }
+        else{
+            mostrarMensagem("danger", conteudoJSON.mensagem);
+        }
+    })
+    .catch((erro) => {
+        mostrarMensagem("danger", "Erro ao excluir o cliente!" + erro);
+    });
+}
+
+function selecionarCurso(id, nome, descricao, cargaHoraria, professor){
+    document.getElementById("id").value = id;
+    document.getElementById("nomeCurso").value = nome;
+    document.getElementById("descricao").value = descricao;
+    document.getElementById("cargaHoraria").value = cargaHoraria;
+    document.getElementById("professor").value = professor;
+
+    document.getElementById("cadastrar").disabled = true;
+    document.getElementById("atualizar").disabled = false;
+    document.getElementById("excluir").disabled = false;
+
+}
+
+function limparFormulario(){
+    const formCurso = document.getElementById("formCurso");
+    formCurso.classList.remove("was-validated");
+    formCurso.reset();
+}
+
+function resetarBotoes(){
+    document.getElementById("cadastrar").disabled = false;
+    document.getElementById("atualizar").disabled = true;
+    document.getElementById("excluir").disabled = true;
+      
+}
+
+function mostrarMensagem(tipo ="success", mensagem = "Mensagem Padrão") {
     const divMensagem = document.getElementById("mensagem");
     divMensagem.innerHTML = `
         <div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
@@ -107,5 +205,21 @@ function mostrarMensagem(tipo ="sucess", mensagem = "Mensagem Padrão") {
     
 }
 
-consultarCurso();
+obterProfessores();
 buscarCursos();
+
+const btnCadastrar = document.getElementById("cadastrar");
+btnCadastrar.onclick = cadastrarCurso;
+
+const btnExcluir = document.getElementById("excluir");
+btnExcluir.onclick = function () {
+    if (confirm("Tem certeza que deseja excluir o Curso?")) {
+        const id = document.getElementById("id").value;
+        if(id){
+            excluirCurso(id);
+            limparFormulario();
+        } else {
+            mostrarMensagem("warning", "Selecione um primeiro um Curso!");
+        }
+    }
+}
